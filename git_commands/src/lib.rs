dd009a9d69bea1422415e74b88e9d0303f7331f5
Tim Hutt <tdhutt@gmail.com>
2021-05-02 15:18:02 +0100

    Coloured error output

--- git_commands/src/lib.rs
+++ git_commands/src/lib.rs
@@ -1,5 +1,6 @@
 
 use std::{io, fmt, path::Path, process::{self, Command}};
+use colored::*;
 
 // Define our error types. These may be customized for our error handling cases.
 // Now we will be able to write our own errors, defer to an underlying error
@@ -7,7 +8,13 @@ use std::{io, fmt, path::Path, process::{self, Command}};
 #[derive(Debug)]
 pub enum Error {
     Io(io::Error),
-    Process(process::Output),
+    Process(ProcessError),
+}
+
+#[derive(Debug)]
+pub struct ProcessError {
+    output: process::Output,
+    command: Vec<String>,
 }
 
 impl fmt::Display for Error {
@@ -17,10 +24,15 @@ impl fmt::Display for Error {
             Self::Process(e) => {
                 write!(
                     f,
-                    "process exited with error code {}\nStdout: {}\nStderr: {}\n",
-                    e.status,
-                    String::from_utf8_lossy(&e.stdout),
-                    String::from_utf8_lossy(&e.stderr),
+                    "{} {}\n{} {:?}\n{} {}\n{} {}\n",
+                    "process exited with exit code".red(),
+                    e.output.status.to_string().red().bold(),
+                    "Command:".bold(),
+                    e.command,
+                    "Stdout:".bold(),
+                    String::from_utf8_lossy(&e.output.stdout),
+                    "Stderr:".bold(),
+                    String::from_utf8_lossy(&e.output.stderr),
                 )
             }
         }
@@ -35,7 +47,6 @@ impl From<io::Error> for Error {
 
 impl std::error::Error for Error {
 }
-
 
 /// Run a git command with the given arguments in the current directory.
 pub fn git_cwd(args: &[&str]) -> Result<process::Output, Error> {
@@ -59,7 +70,10 @@ pub fn git_internal(args: &[&str], working_dir: Option<&Path>) -> Result<process
         .output()?;
 
     if !output.status.success() {
-        return Err(Error::Process(output));
+        return Err(Error::Process(ProcessError {
+            output,
+            command: std::iter::once(&"git").chain(args.iter()).map(|&s| s.to_owned()).collect(),
+        }));
     }
 
     Ok(output)
