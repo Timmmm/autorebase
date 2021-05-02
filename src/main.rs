13f1a5d4bcfc96dcf2dded78a981f711c4442735
Tim Hutt <tdhutt@gmail.com>
2021-05-02 16:56:31 +0100

    Change target_branch to onto

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ use autorebase::{get_repo_path, autorebase};
 /// Automatically pull the master branch and rebase all branches without
 /// upstreams onto it.
 struct CliOptions {
-    /// the target branch (typically "master" or "develop")
+    /// the target branch to pull and rebase onto (typically "master" or "develop")
     #[argh(option, default="String::from(\"master\")")]
-    target_branch: String,
+    onto: String,
 }
 
 fn main() -> Result<()> {
@@ -20,7 +20,7 @@ fn main() -> Result<()> {
     // Find the repo dir in the same way git does.
     let repo_path = get_repo_path()?;
 
-    autorebase(&repo_path, &options.target_branch)?;
+    autorebase(&repo_path, &options.onto)?;
 
     Ok(())
 }
