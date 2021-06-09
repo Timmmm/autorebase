8f295135b613882f3f35624eeb7be90177606525
Tim Hutt <tdhutt@gmail.com>
2021-06-09 11:29:31 +0100

    Add random repo tests (WIP)

---
+++ tests/random.rs
@@ -0,0 +1,50 @@
+mod utils;
+use autorebase::autorebase;
+use utils::*;
+
+// Test randomly generated repos.
+#[test]
+fn random_test_slow() {
+    random_test(true);
+}
+
+#[test]
+fn random_test_fast() {
+    random_test(false);
+}
+
+fn random_test(slow_conflict_detection: bool) {
+    git_fixed_dates();
+
+    let root = random_repo(false);
+
+    let repo = build_repo(&root, Some("master"));
+
+    let repo_dir = repo.path();
+
+    print_git_log_graph(&repo_dir);
+
+    autorebase(repo_dir, "master", slow_conflict_detection).expect("error autorebasing");
+
+    print_git_log_graph(&repo_dir);
+
+    // This doesn't really test anything yet; just makes sure the code doesn't panic.
+}
+
+#[test]
+fn random_test_many_slow() {
+    random_test_many(true);
+}
+
+#[test]
+fn random_test_many_fast() {
+    random_test_many(false);
+}
+
+fn random_test_many(slow_conflict_detection: bool) {
+
+    // This takes about 0.5 seconds per iteration.
+    for _ in 0..10 {
+        random_test(slow_conflict_detection);
+    }
+}
--- tests/utils/random_repo.rs
+++ tests/utils/random_repo.rs
@@ -1,25 +1,66 @@
-use tempfile::TempDir;
+
+use super::CommitDescription;
+use rand::Rng;
+use std::collections::HashSet;
 
 /// Generate a completely random repo with random commits, branches, etc.
 /// If `allow_merges` is true then the repo may contain merge commits.
-fn random_repo(allow_merges: bool) -> TempDir {
+pub fn random_repo(allow_merges: bool) -> CommitDescription {
     if allow_merges {
         todo!();
     }
 
-    // let root =
-    // commit("First")
-    // .write("a.txt", "hello")
-    // .child(
-    //     commit("Second")
-    //     .write("a.txt", "world")
-    //     .branch("master")
-    // )
-    // .child(
-    //     commit("WIP")
-    //     .write("b.txt", "foo")
-    //     .branch("wip")
-    // );
+    // This uses recursion so we need to set a maximum depth to avoid stack overflows.
 
-    todo!();
+    fn randomise_commit(commit: &mut CommitDescription, branches: &mut HashSet<String>, depth: u32) {
+        let mut rng = rand::thread_rng();
+
+        // Randomly set the name, branch, contents, etc.
+        commit.message = format!("Commit {}", rng.gen_range(0..1000));
+        // The filename and contents are drawn from a small distribution to
+        // give a reasonable chance of writing the same file to make conflicts,
+        // or serendipitously eliminating conflicts.
+        commit.changes.insert(format!("{}.txt", rng.gen_range(0..8)), Some(format!("{}", rng.gen_range(0..8))));
+        if rng.gen_bool(0.1) {
+            let branch_name = format!("branch_{}", rng.gen_range(0..1000000));
+            if !branches.contains(&branch_name) {
+                branches.insert(branch_name.clone());
+                commit.branches.push(branch_name);
+            }
+        }
+
+        // Possibly add one or more children. Most commonly we add exactly one
+        // child. Less commonly we add 0, or 2 or more. The expected number
+        // must be less than 1!
+        let num_children = rng.gen_range(0..100);
+        let num_children = if depth > 100 || num_children < 30 {
+            0
+        } else if num_children < 90 {
+            1
+        } else if num_children < 95 {
+            2
+        } else {
+            3
+        };
+
+        // So that we guarantee something is `master`, the first tip will be master.
+        if !branches.contains("master") {
+            commit.branches.push("master".to_owned());
+            branches.insert("master".to_owned());
+        }
+
+
+        for _ in 0..num_children {
+            // Add a child commit
+            commit.children.push(Default::default());
+            randomise_commit(commit.children.last_mut().unwrap(), branches, depth + 1);
+        }
+    }
+
+    let mut root = Default::default();
+    let mut branches = Default::default();
+
+    randomise_commit(&mut root, &mut branches, 0);
+
+    root
 }
